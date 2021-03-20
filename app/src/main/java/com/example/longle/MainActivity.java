a58028af107a72fbc739b58bcecbfe6b75b18e5b
le.lt162349@sis.hust.edu.vn <le.lt162349@sis.hust.edu.vn>
2021-03-21 00:46:38 +0700

    khong co gi dau

--- app/src/main/java/com/example/longle/MainActivity.java
+++ app/src/main/java/com/example/longle/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+// Cmt liunh tinh thoi
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

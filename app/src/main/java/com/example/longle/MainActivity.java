80e0576a162178aaf99b212c444a1320e6d08f05
le.lt162349@sis.hust.edu.vn <le.lt162349@sis.hust.edu.vn>
2021-03-21 00:54:22 +0700

    khong co gi dau

--- app/src/main/java/com/example/longle/MainActivity.java
+++ app/src/main/java/com/example/longle/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 // Cmt liunh tinh thoi
+    //Test nhanh first_change
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
